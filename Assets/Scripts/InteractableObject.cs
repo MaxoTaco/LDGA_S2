da14c44828843b00a843635f9c9904cbd29f2195
Sammy DeMarco <demarco.s@northeastern.edu>
2025-09-23 14:26:24 -0400

    Open Door SFX

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -5,14 +5,27 @@ public class InteractableObject : MonoBehaviour
     public enum InteractableType { Door };
     public InteractableType ObjectType;
 
+    bool doorOpened = false;
+    Animator animator;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
     // add interactable object types with the enum InteractableType, run specific method for each
     public void Interact()
     {
         switch (ObjectType)
         {
             case InteractableType.Door:
-                Animator animator = GetComponent<Animator>();
-                animator.SetTrigger("Open");
+                animator?.SetTrigger("Open");
+
+                if (!doorOpened) audioSource?.Play();
+
+                doorOpened = true;
                 break;
         }
         // add more cases here
